a9ee4ebb0a97ad6f899b36f17cefb873a1f31567
Jasiro500 <roberto500@o2.pl>
2022-04-19 14:47:45 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/RobertSienkiewicz-training/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "my-resources"
+  resource_group_name = azurerm_resource_group.example.name
   # insert required variables here
 }
